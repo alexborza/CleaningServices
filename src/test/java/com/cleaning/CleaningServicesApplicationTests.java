6b7cb568cd6f97eb06bd5b02a253ba51ab339d6d
ZZ02T1826 <alexborza860@yahoo.com>
2021-12-05 13:59:09 +0200

    Initial commit

---
+++ src/test/java/com/cleaning/CleaningServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleaning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleaningServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
