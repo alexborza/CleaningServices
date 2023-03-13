0ad5998ad3e2a6e900d99abde5726eb71e55d98e
ZZ02T1826 <alexborza860@yahoo.com>
2023-03-13 10:37:53 +0200

    added jacoco for measuring coverage and deleted mapstruct dependency from the project

--- src/main/java/com/cleaning/exposition/representation/response/shared/EmployeeAvailableInterval.java
+++ src/main/java/com/cleaning/exposition/representation/response/shared/EmployeeAvailableInterval.java
@@ -3,8 +3,6 @@ package com.cleaning.exposition.representation.response.shared;
 import com.cleaning.exposition.representation.response.appointment.*;
 import lombok.*;
 
-import java.util.*;
-
 @AllArgsConstructor
 @Getter
 public class EmployeeAvailableInterval implements Comparable<EmployeeAvailableInterval> {
@@ -15,18 +13,5 @@ public class EmployeeAvailableInterval implements Comparable<EmployeeAvailableIn
     @Override
     public int compareTo(EmployeeAvailableInterval o) {
         return this.availableInterval.compareTo(o.getAvailableInterval());
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        EmployeeAvailableInterval that = (EmployeeAvailableInterval) o;
-        return Objects.equals(availableInterval, that.availableInterval);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(availableInterval);
     }
 }
